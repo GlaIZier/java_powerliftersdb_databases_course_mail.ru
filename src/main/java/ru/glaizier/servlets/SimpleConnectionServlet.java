3243a49c091a859216731aa3edccfd47c9a5780b
GlaIZier <kh.mv@bk.ru>
2016-05-22 16:13:00 +0300

    Freemarker has been added

--- src/main/java/ru/glaizier/servlets/SimpleConnectionServlet.java
+++ src/main/java/ru/glaizier/servlets/SimpleConnectionServlet.java
@@ -1,27 +1,26 @@
 package ru.glaizier.servlets;
 
-import ru.glaizier.simple.SimpleConnection;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class SimpleConnectionServlet extends HttpServlet {
 
     public static final String CONTENT_TYPE = "text/html; charset=UTF-8";
+    public static final String SIMPLE_TEMPLATE_NAME = "simple.ftl";
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
         response.setContentType(CONTENT_TYPE);
-        try {
-            response.getWriter().println("Hello from Simple Servlet! " + new SimpleConnection().getConnection().getSchema());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        response.getWriter().close();
+//        try {
+//            response.getWriter().println("Hello from Simple Servlet! " + new SimpleConnection().getConnection().getSchema());
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        response.getWriter().close();
+        request.getRequestDispatcher(SIMPLE_TEMPLATE_NAME).forward(request, response);
     }
 }
